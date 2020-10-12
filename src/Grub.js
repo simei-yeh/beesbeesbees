60fffd978b50164e6310f343158acad88f24cb2a
Simei Yeh <simei.yeh@gmail.com>
2020-10-12 10:57:49 -0700

    Created Bee subclass and Grub superclass

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,21 @@
+//Grub, pseudoclassical style
 var Grub = function() {
+  this.age = 0;
+  this.color = "pink";
+  this.food = "jelly";
 };
+
+Grub.prototype.eat = function() {
+};
+
+
+//Bee, pseudoclassical style
+var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "keep on growing";
+}
+
+Bee.prototype = Object.create(Grub.prototype);
 
