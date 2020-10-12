f3cd6edd985769f2bf8133dbc590a91ac0e98bb6
Simei Yeh <simei.yeh@gmail.com>
2020-10-12 11:18:27 -0700

    Updated Grub superclass and Bee subclasses

--- src/Grub.js
+++ src/Grub.js
@@ -7,15 +7,3 @@ var Grub = function() {
 
 Grub.prototype.eat = function() {
 };
-
-
-//Bee, pseudoclassical style
-var Bee = function() {
-  Grub.call(this);
-  this.age = 5;
-  this.color = "yellow";
-  this.job = "keep on growing";
-}
-
-Bee.prototype = Object.create(Grub.prototype);
-
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
+//HoneyMakerBee, pseudoclassical style
 var HoneyMakerBee = function() {
 };
 
+var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = "make honey";
+  this.honeyPot = 0;
+}
+
+HoneyMakerBee.prototype = Object.create(Grub.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot += 1;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot -= 1;
+};
